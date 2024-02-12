7f6b8763853ae3e3b75c36eb69854c2ea49d58f8
Roman Ogolikhin <ogolikhin@hotmail.com>
2024-02-12 23:34:25 +0300

    Added dto validation.

--- src/MSAccessHttp/Controllers/CommandDto.cs
+++ src/MSAccessHttp/Controllers/CommandDto.cs
@@ -1,17 +1,23 @@
-﻿using System.Data;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Data;
 using System.Text.Json.Serialization;
 
 namespace MSAccessHttp.Controllers;
 
 public record CommandDto(
+    [Required]
     string ConnectionString,
+    [Required]
     string Query,
     List<ParameterDto>? Parameters
 );
 
 public record ParameterDto(
+    [Required]
     string ParameterName,
     [property:JsonConverter(typeof(JsonStringEnumConverter<DbType>))]
+    [Required]
     DbType DbType,
+    [Required]
     string? Value
 );
